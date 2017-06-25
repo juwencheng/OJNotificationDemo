cf739b114772a2ed973abade0d3748f71d81cd57
juwenz <juwenz@icloud.com>
2017-06-25 10:50:15 +0800

    Initial Commit

---
+++ OJNotificationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OJNotificationDemo
+//
+//  Created by Juwencheng on 25/06/2017.
+//  Copyright © 2017 Owen Ju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OJNotificationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OJNotificationDemo
+//
+//  Created by Juwencheng on 25/06/2017.
+//  Copyright © 2017 Owen Ju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
