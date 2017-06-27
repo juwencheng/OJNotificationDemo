d7b36ca78f2610c492adb3352b5c3ed7dff0406c
juwenz <juwenz@icloud.com>
2017-06-27 21:21:00 +0800

    修改content约束，添加滑动事件
    修改content leading 和 trailing 属性。添加滑动时间监听

--- OJNotificationDemo/OJNotification/OJNotificationWindow.m
+++ OJNotificationDemo/OJNotification/OJNotificationWindow.m
@@ -25,12 +25,12 @@
 
 @end
 
-// TODO: 处理 window 旋转后的布局
-
 @interface OJNotificationWindow ()
 
 @property (nonatomic, strong) OJNotificationView *content;           ///< 内部的通知视图
-@property (nonatomic, strong) NSLayoutConstraint *topConst;          ///< content 和 window 顶部的约束，用于动画显示
+@property (nonatomic, strong) NSLayoutConstraint *contentTopConst;          ///< content 和 window 顶部的约束，用于动画显示
+@property (nonatomic, strong) NSLayoutConstraint *contentLeadingConst;          ///< content 和 window 左边的约束，用于动画显示
+@property (nonatomic, strong) NSLayoutConstraint *contentTrailingConst;          ///< content 和 window 右边的约束，用于动画显示
 
 @property (nonatomic, assign) OJNotificationPresentState presentState; ///< 展示状态
 @property (nonatomic, assign) NSTimeInterval nw_animationDuration;     ///< 展示动画时长
@@ -41,6 +41,16 @@
 @property (nonatomic, strong) OJNotificationModel *presentingNotification;   ///< 展示的message，用于判断是否需要更新当前通知
 
 @property (nonatomic, strong) UIVisualEffectView *blurView;
+
+@end
+
+@interface OJNotificationWindow (Gesture) <UIGestureRecognizerDelegate>
+
+- (void)addGesture;
+
+- (void)swipeLeft;
+
+- (void)swipeNormal;
 
 @end
 
@@ -85,6 +95,7 @@
 //    self.alpha = 0.9;
     [self setupSubviews];
     [self registOrientationNotification];
+    [self addGesture];
 }
 
 - (void)setupSubviews {
@@ -92,12 +103,17 @@
     [self addSubview:self.content];
     
     self.content.translatesAutoresizingMaskIntoConstraints = NO;
-    for (NSString *constStr in @[@"H:|-(16)-[alert]-(16)-|",@"V:[alert]-(>=8)-|"]) {
+    for (NSString *constStr in @[@"V:[alert]-(>=8)-|"]) {
         [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:constStr options:0 metrics:nil views:@{@"alert":self.content}]];
     }
     
-    self.topConst = [NSLayoutConstraint constraintWithItem:self.content attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1 constant:-200];
-    [self addConstraint:self.topConst];
+    
+    self.contentTopConst = [NSLayoutConstraint constraintWithItem:self.content attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1 constant:-200];
+    [self addConstraint:self.contentTopConst];
+    self.contentLeadingConst = [NSLayoutConstraint constraintWithItem:self.content attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeading multiplier:1 constant:16];
+    [self addConstraint:self.contentLeadingConst];
+    self.contentTrailingConst = [NSLayoutConstraint constraintWithItem:self.content attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTrailing multiplier:1 constant:-16];
+    [self addConstraint:self.contentTrailingConst];
     
     self.blurView.translatesAutoresizingMaskIntoConstraints = NO;
     [self addConstraint:[NSLayoutConstraint constraintWithItem:self.blurView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.content attribute:NSLayoutAttributeTop multiplier:1 constant:-16]];
@@ -165,7 +181,7 @@
     if (first) {
         [self layoutIfNeeded];
     }
-    self.topConst.constant = 16;
+    self.contentTopConst.constant = 16;
     [UIView animateWithDuration:self.nw_animationDuration animations:^{
         [self setNeedsUpdateConstraints];
         [self layoutIfNeeded];
@@ -184,13 +200,19 @@
 }
 
 - (void)dismissNotification {
-    self.topConst.constant = -200;
+    if ([self.nw_animationTimer isValid]) {
+        [self.nw_animationTimer invalidate];
+    }
+    self.contentTopConst.constant = -200;
     [UIView animateWithDuration:self.nw_animationDuration animations:^{
         [self setNeedsUpdateConstraints];
         [self layoutIfNeeded];
     } completion:^(BOOL finished) {
         _presentState = OJNotificationPresentStateFinished;
         [[UIApplication sharedApplication].delegate.window makeKeyAndVisible];
+        
+        // 恢复左右约束
+        [self swipeNormal];
     }];
     // 不设置会导致crash，估计是野指针的问题
     self.nw_animationTimer = nil;
@@ -229,5 +251,87 @@
     return _blurView;
 }
 
+
+@end
+
+
+@implementation OJNotificationWindow (Gesture)
+
+- (void)addGesture {
+    // 向上滑动隐藏手势
+    UISwipeGestureRecognizer *swipeUp = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    swipeUp.direction = UISwipeGestureRecognizerDirectionUp;
+    [self addGestureRecognizer:swipeUp];
+    
+    // 向左滑动显示菜单
+    UISwipeGestureRecognizer *swipeLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    swipeLeft.direction = UISwipeGestureRecognizerDirectionLeft;
+    [self addGestureRecognizer:swipeLeft];
+    
+    // 向右滑动显示菜单
+    UISwipeGestureRecognizer *swipeRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    swipeRight.direction = UISwipeGestureRecognizerDirectionRight;
+    [self addGestureRecognizer:swipeRight];
+}
+
+- (void)handleSwipe:(UISwipeGestureRecognizer *)gesture {
+    switch (gesture.direction) {
+        case UISwipeGestureRecognizerDirectionUp:
+            [self dismissNotification];
+            break;
+        case UISwipeGestureRecognizerDirectionLeft:
+        {
+            [self swipeLeft];
+            [UIView animateWithDuration:0.25 animations:^{
+                [self setNeedsUpdateConstraints];
+                [self layoutIfNeeded];
+            }];
+        }
+            break;
+        case UISwipeGestureRecognizerDirectionRight:
+        {
+            [self swipeNormal];
+            [UIView animateWithDuration:0.25 animations:^{
+                [self setNeedsUpdateConstraints];
+                [self layoutIfNeeded];
+            }];
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)swipeLeft {
+    self.contentLeadingConst.constant -= 200;
+    self.contentTrailingConst.constant -= 200;
+}
+
+- (void)swipeNormal {
+    self.contentLeadingConst.constant = 16;
+    self.contentTrailingConst.constant = -16;
+}
+
+- (void)handlePan:(UIPanGestureRecognizer *)gesture {
+    /*
+    CGPoint point = [sender translationInView:_testView];
+    //    sender.view.transform = CGAffineTransformMake(1, 0, 0, 1, point.x, point.y);
+    
+    //平移一共两种移动方式
+    //第一种移动方法:每次移动都是从原来的位置移动
+    //    sender.view.transform = CGAffineTransformMakeTranslation(point.x, point.y);
+    
+    //第二种移动方式:以上次的位置为标准(移动方式 第二次移动加上第一次移动量)
+    sender.view.transform = CGAffineTransformTranslate(sender.view.transform, point.x, point.y);
+    //增量置为o
+    [sender setTranslation:CGPointZero inView:sender.view];
+    
+    _testView.backgroundColor = [UIColor colorWithRed:arc4random()%256/255.0 green:arc4random()%256/255.0 blue:arc4random()%256/255.0 alpha:1];
+    */
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return YES;
+}
 
 @end
